d4ad93d51cad7cf36c4fe595026a7b7807e23a8b
Prasham1710 <jainprasham17@gmail.com>
2023-02-22 03:16:58 +0530

    post Message

---
+++ server/controllers/posts.js
@@ -0,0 +1,2 @@
+export const getPosts = (req, res) => {
+    res.send('THIS WORKS!');}
--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+const app = express();
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://prasham1710:memory@cluster0.tjy5vu5.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+ //http://localhost:5000/posts
