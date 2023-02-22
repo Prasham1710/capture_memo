876f8cc94375ece9f4df0a1545a51790128ad2e0
Prasham1710 <jainprasham17@gmail.com>
2023-02-23 02:40:19 +0530

    redux package

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import {applyMiddleware , componse} from 'redux';
+import { configureStore } from 'redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
