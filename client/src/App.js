749561201886d50ac3a1ac65b7f6cdbd355d050e
Prasham1710 <jainprasham17@gmail.com>
2023-02-23 00:55:00 +0530

    styles.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Container, AppBar, Typography, Grow ,Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Form/Form.js';
 import memories from './images/memories.png'
 const App = () => {
   return (
     <Container maxidth="lg">
       <AppBar position='static' color='inherit'>
         <Typography variant='h2' align='centre'>Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
-      </AppBar></Container>
+        <img src={memories} alt="memories" height="400" width= "600"/>
+      </AppBar>
+      <Grow in >
+        <Container>
+          <Grid container justify-space-between alignItems='stretch' spacing={3}> 
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+        </Grow></Container>
   )
 }
 
