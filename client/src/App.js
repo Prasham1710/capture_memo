d5cb43c2a43ab0dd01ef9291fda189b4efcb00c1
Prasham1710 <jainprasham17@gmail.com>
2023-02-23 02:12:34 +0530

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import { Container, AppBar, Typography, Grow ,Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 import memories from './images/memories.png'
+import useStyles from './styles';
+
 const App = () => {
+    const classes = useStyles();
+
   return (
     <Container maxidth="lg">
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='centre'>Memories</Typography>
-        <img src={memories} alt="memories" height="400" width= "600"/>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography  className= {classes.heading}variant='h2' align='centre'>Memories</Typography>
+        <img className= {classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in >
         <Container>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Post from './Post/Post.js'
+import useStyles from './styles';
 const Posts = () => {
+    const classes = useStyles();
   return (
     <div>
         <h1>Posts</h1>
