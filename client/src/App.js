8586b99aa3becb79bd14461e5632d5ec9b9f5d01
Prasham1710 <jainprasham17@gmail.com>
2023-02-24 02:37:02 +0530

    form completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect}from 'react'
 import { Container, AppBar, Typography, Grow ,Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
-import memories from './images/memories.png'
+import memories from './images/memories.png';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 import { getPosts } from './actions/posts';
